54c1541af8ea1acd53cc5e74967a99bc72495b6b
leyfon <leyfon@leyfon-K52Jc>
2014-10-31 17:23:14 +0100

    hahaha

--- HelloWorld/src/com/example/helloworld/DisplayMessageActivity.java
+++ HelloWorld/src/com/example/helloworld/DisplayMessageActivity.java
@@ -1,9 +1,11 @@
 package com.example.helloworld;
 
+import android.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends Activity {
@@ -20,16 +22,24 @@ public class DisplayMessageActivity extends Activity {
 	}
 
 
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+//
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		// Handle action bar item clicks here. The action bar will
+//		// automatically handle clicks on the Home/Up button, so long
+//		// as you specify a parent activity in AndroidManifest.xml.
+//		int id = item.getItemId();
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
+//		return super.onOptionsItemSelected(item);
+//	}
+	
+	
+	//TODO NOT FINISHED
+	public void onStared(){
+		int myStringArray = 5; 
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		        android.R.layout.simple_list_item_1, myStringArray);	
 	}
 }
